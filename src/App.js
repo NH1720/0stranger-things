946a72965c61d2cb52571c7ae82c9dd0d4bc4407
Nathan <nathan.wthamilton48@gmail.com>
2022-10-27 23:08:17 -0500

    fetching posts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {Home, Posts} from "./components"
 import {Route, Switch, Link} from "react-router-dom";
+import { fetchPosts } from "./api/api";
 
 const App = () => {
+    const {posts, setPosts} = useState([]);
+
+    useEffect(() => {
+        const getPosts = async() => {
+            try {
+                const result = await fetchPosts()
+                setPosts(result)
+            } catch(error) {
+                console.error(error)
+            }
+        }
+        getPosts();
+    }, [])
     return (
         <div>
             <nav>
@@ -14,7 +28,7 @@ const App = () => {
                     <Home />
                 </Route>
                 <Route path="/posts">
-                    <Posts />
+                    <Posts post={posts}/>
                 </Route>
             </Switch>
         </div>
