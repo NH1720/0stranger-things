fad054319493ccd4e8dc16f4841f9138403487f2
Nathan <nathan.wthamilton48@gmail.com>
2022-10-27 20:32:58 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import {Home} from "./components"
+import {Home, Posts} from "./components"
 import {Route, Switch, Links} from "react-router-dom";
 
 const App = () => {
     return (
         <div>
-            <nav></nav>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/">Posts</Link>
+            </nav>
             <Switch>
                 <Route exact path="/">
                     <Home />
+                </Route>
+                <Route path="/posts">
+                    <Posts />
                 </Route>
             </Switch>
         </div>
---
+++ src/components/Posts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Posts = () => {
+    return <div>List of Posts</div>
+};
+
+export default Posts;
